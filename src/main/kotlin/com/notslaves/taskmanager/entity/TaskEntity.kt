af575965abaf2b30099da11b35003d2940460600
Nelite <neliitsme14@gmail.com>
2022-10-13 16:39:17 +0300

    feat: add fields to `TaskEntity`

--- src/main/kotlin/com/notslaves/taskmanager/entity/TaskEntity.kt
+++ src/main/kotlin/com/notslaves/taskmanager/entity/TaskEntity.kt
@@ -10,4 +10,19 @@ class TaskEntity {
     @GeneratedValue
     @Column(name = "id")
     var id: Int? = null
+
+    @Column(name = "name")
+    var name: String? = null
+
+    @Lob
+    @Column(name = "description")
+    var description: String? = null
+
+    @ManyToOne()
+    @JoinColumn(name = "status_id", referencedColumnName = "id")
+    var status: TaskStatusEntity? = null
+
+    @ManyToOne(cascade = [CascadeType.REMOVE])
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    var user: UserEntity? = null
 }
