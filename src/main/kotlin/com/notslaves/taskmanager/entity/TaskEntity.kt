b922512d2cabdbe7a8804dce90ecd9889be207a0
Nelite <neliitsme14@gmail.com>
2022-10-13 19:48:12 +0300

    feat: add a timestamp to `TaskEntity`

--- src/main/kotlin/com/notslaves/taskmanager/entity/TaskEntity.kt
+++ src/main/kotlin/com/notslaves/taskmanager/entity/TaskEntity.kt
@@ -18,7 +18,11 @@ class TaskEntity {
     @Column(name = "description")
     var description: String? = null
 
-    @ManyToOne()
+    @Column(name = "created", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    @Temporal(TemporalType.TIMESTAMP)
+    var created: Date? = null
+
+    @ManyToOne
     @JoinColumn(name = "status_id", referencedColumnName = "id")
     var status: TaskStatusEntity? = null
 
