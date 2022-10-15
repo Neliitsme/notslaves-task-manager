512dd2fa4ae36fd062854e60091ce8516a4c9c46
Nelite <neliitsme14@gmail.com>
2022-10-15 12:29:54 +0300

    feat: add implement `TaskStatusService` functions, call them in controller

--- src/main/kotlin/com/notslaves/taskmanager/controller/TaskStatusController.kt
+++ src/main/kotlin/com/notslaves/taskmanager/controller/TaskStatusController.kt
@@ -1,21 +1,34 @@
 package com.notslaves.taskmanager.controller
 
+import com.notslaves.taskmanager.model.TaskStatus
 import com.notslaves.taskmanager.service.TaskStatusService
+import org.springframework.http.HttpStatus
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.server.ResponseStatusException
 
 @RestController
 @RequestMapping("/api/v1/taskstatuses")
 class TaskStatusController(private val service: TaskStatusService) {
     // Todo
     @GetMapping
-    fun getAllStatuses() = null
+    fun getAllStatuses(): List<TaskStatus> = service.getAllStatuses()
 
     @GetMapping("/{id}")
-    fun getStatus(@PathVariable("id") id: Int) = null
+    fun getStatus(@PathVariable("id") id: Int): TaskStatus? =
+        service.getStatus(id) ?: throw ResponseStatusException(
+            HttpStatus.NOT_FOUND,
+            "Task status with id $id not found"
+        )
+
+    @PostMapping
+    fun createStatus(@RequestBody @Validated taskStatus: TaskStatus): TaskStatus = service.createStatus(taskStatus)
 
     @DeleteMapping("/{id}")
-    fun deleteStatus(@PathVariable("id") id: Int) = null
+    fun deleteStatus(@PathVariable("id") id: Int) = service.deleteStatus(id)
 
-    @PatchMapping("/{id}")
-    fun modifyStatus(@PathVariable("id") id: Int) = null
+    // Todo: Could make a null check by making and id put request
+    // @PutMapping("/{id}")
+    @PutMapping()
+    fun modifyStatus(@RequestBody @Validated taskStatus: TaskStatus): TaskStatus? = service.modifyStatus(taskStatus)
 }
--- src/main/kotlin/com/notslaves/taskmanager/service/TaskStatusService.kt
+++ src/main/kotlin/com/notslaves/taskmanager/service/TaskStatusService.kt
@@ -1,7 +1,33 @@
 package com.notslaves.taskmanager.service
 
+import com.notslaves.taskmanager.model.TaskStatus
+import com.notslaves.taskmanager.repository.TaskStatusRepository
 import org.springframework.stereotype.Service
+import java.lang.IllegalArgumentException
 
 @Service
-class TaskStatusService {
+class TaskStatusService(
+    private val mapper: TaskStatusMapper,
+    private val repository: TaskStatusRepository
+) {
+    fun getAllStatuses(): List<TaskStatus> = repository.findAll().map { mapper.entityToModel(it) }
+
+    fun getStatus(id: Int): TaskStatus? = repository.findById(id)
+        .orElse(null)?.let { mapper.entityToModel(it) }
+
+    fun deleteStatus(id: Int) = repository.deleteById(id)
+
+    fun createStatus(taskStatus: TaskStatus): TaskStatus {
+        val entity = mapper.modelToEntity(taskStatus)
+        return mapper.entityToModel(repository.save(entity))
+    }
+
+    // Todo: Modify not saving
+    fun modifyStatus(taskStatus: TaskStatus): TaskStatus? {
+        val entity =
+            taskStatus.id?.let {
+                repository.findById(it).orElse(null)?.let { mapper.modelToEntity(taskStatus) }
+            }
+        return entity?.let { mapper.entityToModel(entity) } ?: throw IllegalArgumentException()
+    }
 }
